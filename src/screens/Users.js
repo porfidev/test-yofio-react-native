9062616e163f6309dc7233dd00bc63fa6b6e5e81
Porfirio Chávez <elporfirio@192.168.1.7>
2022-03-21 08:28:17 -0600

    checkpoint

--- src/screens/Users.js
+++ src/screens/Users.js
@@ -1,10 +1,19 @@
 import {useFocusEffect} from '@react-navigation/native';
 import React, {useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
-import {Button, FlatList, Pressable, Text, View} from 'react-native';
+import {
+  Button,
+  FlatList,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import {addUser, getAllUsers} from '../actions/UsersActions.js';
+import {COLORS} from '../theme/colors.js';
+import {STYLES} from '../theme/styles.js';
 
 const UsersScreen = ({navigation, getAllUsers, users}) => {
   const [loading, setLoading] = useState(false);
@@ -27,30 +36,52 @@ const UsersScreen = ({navigation, getAllUsers, users}) => {
 
   const renderUser = ({item}) => {
     return (
-      <Pressable onPress={() => navigation.navigate('Detalle', {user: item})}>
-        <Text>{item.name}</Text>
+      <Pressable
+        onPress={() => navigation.navigate('UserDetail', {user: item})}>
+        <View
+          style={{
+            paddingVertical: 10,
+            paddingHorizontal: 20,
+            backgroundColor: COLORS.cardBackground,
+            marginVertical: 4,
+            borderRadius: 10,
+          }}>
+          <Text style={{color: COLORS.cardText, fontSize: 18}}>
+            {item.name}
+          </Text>
+        </View>
       </Pressable>
     );
   };
 
   return (
-    <View>
-      <Text>Usuarios {users.length}</Text>
+    <View style={styles.mainContainer}>
       <FlatList
         data={users}
         renderItem={renderUser}
         refreshing={loading}
         onRefresh={init}
       />
-      <Button
-        title="Ir a Detalle"
-        onPress={() => navigation.navigate('Detalle')}
-      />
     </View>
   );
 };
 
-UsersScreen.propTypes = {};
+const styles = StyleSheet.create({
+  mainContainer: {
+    backgroundColor: COLORS.background,
+    height: '100%',
+  },
+  dateTimeContainer: {
+    ...STYLES.card,
+  },
+  mapContainer: {
+    height: 300,
+    margin: 20,
+    borderColor: COLORS.primary,
+    borderWidth: 1,
+    elevation: 10,
+  },
+});
 
 const mapStateToProps = state => ({
   users: state.users,
